04fa829ce8a100965b86585cda696a0dba01beb7
J C C Element <Jamie@campus-10-41-31-222.wireless.leeds.ac.uk>
2015-11-30 10:43:34 +0000

    - Created structure of application by adding new view controllers

---
+++ Connect Four Advance/MenuViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MenuViewController.swift
+//  Connect Four Advance
+//
+//  Created by J C C Element on 30/11/2015.
+//  Copyright © 2015 DanAndJamie. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("view loaded")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
