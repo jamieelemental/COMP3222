d7599eaf16b5669d68140499aefd4593eed02ca2
Daniel Nuttall <danielnuttall@daniels-mac-mini.local>
2015-11-30 19:04:20 +0000

    Updated SinglePayer Mode, with a basic interpretation of the physics. - No logic in the game whatsoever yet.

--- Connect Four Advance/SinglePlayerScene.swift
+++ Connect Four Advance/SinglePlayerScene.swift
@@ -7,17 +7,81 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 class SinglePlayerScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    
+    // Create catogorys for elements of the scene
+    let redCategoryName = "red"
+    let blueCategoryName = "blue"
+    let buttonFactoryCategoryName = "buttonFactory"
+    let wallCategoryName = "wall"
+    // Create Music player
+    var bgMusicPlayer = AVAudioPlayer()
+    
+    
+    override init(size: CGSize) {
+        super.init(size: size)
         
-        self.addChild(myLabel)
+        //let bgMusicURL = NSBundle.mainBundle().URLForResource("BGMusic", withExtension: "mp3")
+        //bgMusicPlayer = AVAudioPlayer(contentsOfURL: bgMusicURL, error: nil)
+        
+        
+        let backgroundImage = SKSpriteNode(imageNamed: "bg")
+        backgroundImage.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2)
+        self.addChild(backgroundImage)
+        
+        // Control Gravity
+        self.physicsWorld.gravity = CGVectorMake(0, -1)
+        
+        // set border of world
+        let screenBorder = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody = screenBorder
+        self.physicsBody?.friction = 1
+        
+        // Code to create columns.
+        //let rows = 1
+        let walls = 7
+        let wallWidth = SKSpriteNode(imageNamed: "wall").size.width
+        let padding:Float = 40
+        
+        
+        // Generate the walls
+        let offset:Float = (Float(self.frame.size.width) - (Float(wallWidth) * Float(walls) + padding * (Float(walls) - 1 ) ) ) / 2
+        
+        for index in 1 ... walls {
+            let wall = SKSpriteNode(imageNamed: "wall")
+            
+            let calc1:Float = Float(index) - 0.5
+            let calc2:Float = Float(index) - 1
+            
+            wall.position = CGPointMake(CGFloat(calc1 * Float(wall.frame.size.width) + calc2 * padding + offset), 1)
+            
+            wall.physicsBody = SKPhysicsBody(rectangleOfSize: wall.frame.size)
+            wall.physicsBody?.allowsRotation
+            wall.name = wallCategoryName
+            wall.physicsBody?.dynamic = false
+            
+            self.addChild(wall)
+        
     }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+    
+//    override func didMoveToView(view: SKView) {
+//        
+//    }
+    
+    
+    
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
@@ -25,17 +89,20 @@ class SinglePlayerScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let red = SKSpriteNode(imageNamed: "blue")
+            red.name = redCategoryName
+            red.position = location
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            //let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            //red.runAction(SKAction.repeatActionForever(action))
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            self.addChild(red)
+            red.physicsBody = SKPhysicsBody(circleOfRadius: red.frame.size.width/2)
+            red.physicsBody?.friction = 0
+            red.physicsBody?.restitution = 0.1
+            red.physicsBody?.allowsRotation = false
             
-            self.addChild(sprite)
         }
     }
     
--- Connect Four Advance/SinglePlayerViewController.swift
+++ Connect Four Advance/SinglePlayerViewController.swift
@@ -11,29 +11,6 @@ import SpriteKit
 
 
 class SinglePlayerViewController: UIViewController {
-    
-  //  override func viewDidLoad() {
-  //      super.viewDidLoad()
-        
-        
-        
-        //let scene = SinglePlayerScene (size){
-            // Configure the view.
-        //    let skView = self.view as! SKView
-        //    skView.showsFPS = true
-        //    skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-        //    skView.ignoresSiblingOrder = true
-        //
-        //    /* Set the scale mode to scale to fit the window */
-        //    scene.scaleMode = .AspectFill
-        //
-        //    skView.presentScene(scene)
-        //}
-    
-    // Do any additional setup after loading the view.
-//}
 
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -50,16 +27,5 @@ class SinglePlayerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
     
 }
