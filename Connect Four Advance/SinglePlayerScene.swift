6b652194ecff6d83daabdc4d9c5d98e28e214b41
Daniel Nuttall <danielnuttall@daniels-mac-mini.local>
2015-12-01 13:39:00 +0000

    Basic playable version! - SinglePlayer

--- Connect Four Advance/SinglePlayerScene.swift
+++ Connect Four Advance/SinglePlayerScene.swift
@@ -44,6 +44,8 @@ class SinglePlayerScene: SKScene {
         let walls = 7
         let wallWidth = SKSpriteNode(imageNamed: "wall").size.width
         let padding:Float = 40
+        // Code to generate ball factorys
+        let ballFacts = 6
         
         
         // Generate the walls
@@ -58,15 +60,39 @@ class SinglePlayerScene: SKScene {
             wall.position = CGPointMake(CGFloat(calc1 * Float(wall.frame.size.width) + calc2 * padding + offset), 1)
             
             wall.physicsBody = SKPhysicsBody(rectangleOfSize: wall.frame.size)
-            wall.physicsBody?.allowsRotation
+            wall.physicsBody?.allowsRotation = false
             wall.name = wallCategoryName
             wall.physicsBody?.dynamic = false
             wall.zPosition = 1.0
-            
+
             self.addChild(wall)
+    }
+        
+        // Generate Ball Factorys
+        for index in 1 ... ballFacts {
+            let wall = SKSpriteNode(imageNamed: "wall")
+            
+            let calc1:Float = Float(index) - 0.5
+            let calc2:Float = Float(index) - 1
+            
+            //Ball Factory code
+            let ballMaker = SKSpriteNode(imageNamed:"ball")
+            ballMaker.name = buttonFactoryCategoryName
+            ballMaker.physicsBody?.allowsRotation = false
+            ballMaker.physicsBody?.dynamic = false
+            ballMaker.size.width = CGFloat(39)
+            ballMaker.size.height = CGFloat(39)
+            ballMaker.position = CGPointMake(CGFloat(calc1 * Float(wall.frame.size.width) + calc2 * padding + offset + 30), 270)
+            ballMaker.zPosition = 1.0
+            
+            self.addChild(ballMaker)
+            
+        }
+        
         
     }
-    }
+    
+    
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -82,28 +108,34 @@ class SinglePlayerScene: SKScene {
         /* Called when a touch begins */
         
         for touch in touches {
-            let location = touch.locationInNode(self)
-            var red = SKSpriteNode(imageNamed:"red")
-            if turn == 0 {
-                red = SKSpriteNode(imageNamed:"blue")
-                turn = 1
-            } else if turn == 1{
-                red = SKSpriteNode(imageNamed:"red")
-                turn = 0
-           }
-            
-            red.name = redCategoryName
-            red.position = location
-            red.size.width = CGFloat(39)
-            red.size.height = CGFloat(39)
-            red.zPosition = 1.0
-            
-            red.physicsBody = SKPhysicsBody(circleOfRadius: red.frame.size.width/2)
-            red.physicsBody?.friction = 0
-            red.physicsBody?.restitution = 0.1
-            red.physicsBody?.allowsRotation = false
-            self.addChild(red)
-            
+        let positionInScene = touch.locationInNode(self)
+        let touchedNode = self.nodeAtPoint(positionInScene)
+        
+        if let name = touchedNode.name
+        {
+            if name == "buttonFactory"
+            {
+                var red = SKSpriteNode(imageNamed:"red")
+                if turn == 0 {
+                        red = SKSpriteNode(imageNamed:"blue")
+                        turn = 1
+                } else if turn == 1{
+                        red = SKSpriteNode(imageNamed:"red")
+                        turn = 0
+                }
+                red.name = redCategoryName
+                red.position = positionInScene
+                red.size.width = CGFloat(39)
+                red.size.height = CGFloat(39)
+                red.zPosition = 1.0
+                
+                red.physicsBody = SKPhysicsBody(circleOfRadius: red.frame.size.width/2)
+                red.physicsBody?.friction = 0
+                red.physicsBody?.restitution = 0.1
+                red.physicsBody?.allowsRotation = false
+                self.addChild(red)
+            }
+        }
         }
     }
     
