6a267bf2e5ecc2865b45f0e2f0449ce05ac953c0
J C C Element <Jamie@campus-10-41-30-65.wireless.leeds.ac.uk>
2015-12-04 10:04:14 +0000

    Added walls - Credit to Daniel Nuttall for physics code Added row to stop balls dropping out Centered the grid

--- Connect Four Advance/TwoPlayerScene.swift
+++ Connect Four Advance/TwoPlayerScene.swift
@@ -20,8 +20,9 @@ class TwoPlayerScene: SKScene {
         let screenBorder = SKPhysicsBody(edgeLoopFromRect: self.frame)
         self.physicsBody = screenBorder
         self.physicsBody?.friction = 1
+        
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -31,6 +32,7 @@ class TwoPlayerScene: SKScene {
         print("Two player loaded")
         scaleMode = .ResizeFill
         self.backgroundColor = UIColor.whiteColor()
+        drawGrid()
         
         
     }
@@ -48,7 +50,7 @@ class TwoPlayerScene: SKScene {
                 Node.fillColor = UIColor.yellowColor()
                 playerTurn = 1
             }
-            
+                
             else if playerTurn == 1{
                 Node.fillColor = UIColor.redColor()
                 playerTurn = 0
@@ -61,7 +63,7 @@ class TwoPlayerScene: SKScene {
             sprite.physicsBody?.restitution = 0.1
             sprite.physicsBody?.allowsRotation = false
             sprite.zPosition = 1.0
-
+            
             self.addChild(sprite)
         }
     }
@@ -72,6 +74,72 @@ class TwoPlayerScene: SKScene {
     
     func drawGrid()
     {
+        
+        
+        // Code to create columns.
+        let walls = 8
+        let padding:Float = 52
+        
+        
+        
+        // Generate the walls
+        let offset:Float = (Float(self.frame.size.width) - (Float(10) * Float(walls) + padding * (Float(walls) - 1 ) ) ) / 2
+        
+        
+        for index in 1 ... walls {
+            
+            
+            let wallNode = SKShapeNode(rectOfSize: CGSize(width: (self.frame.width / 100), height: self.frame.size.height/2))
+            wallNode.fillColor = UIColor.blueColor()
+            
+            
+            
+            let wall = SKSpriteNode()
+            
+            
+            wall.physicsBody = SKPhysicsBody(rectangleOfSize: wallNode.frame.size)
+            
+            wall.physicsBody?.friction = 0
+            wall.physicsBody?.allowsRotation = false
+            wall.physicsBody?.dynamic = false
+            wall.zPosition = 1.0
+            
+            
+            //let calc1:Float = Float(index) - 0.5
+            //let calc2:Float = Float(index) - 1
+            
+            //wall.position = CGPointMake(CGFloat(calc1 * Float(wall.frame.size.width) + calc2 * padding + offset), 30)
+            wall.position = CGPointMake( (CGFloat(offset) + CGFloat(index) * CGFloat(padding)), self.frame.height/2)
+            
+            
+            wall.addChild(wallNode)
+            self.addChild(wall)
+            
+            
+        }
+        
+        
+        let width = (CGFloat(offset) + CGFloat(1) * CGFloat(padding)) - (CGFloat(offset) + CGFloat(8) * CGFloat(padding))
+        
+        let baseNode = SKShapeNode(rectOfSize: CGSize(width: width, height: self.frame.width/100))
+        baseNode.fillColor = UIColor.blueColor()
+        
+        
+        
+        let base = SKSpriteNode()
+        
+        
+        base.physicsBody = SKPhysicsBody(rectangleOfSize: baseNode.frame.size)
+        
+        base.physicsBody?.friction = 0
+        base.physicsBody?.allowsRotation = false
+        base.physicsBody?.dynamic = false
+        base.zPosition = 1.0
+        
+        base.position = CGPointMake(self.frame.width/2 + CGFloat(self.frame.width/60),  self.frame.height/4)
+        base.addChild(baseNode)
+        
+        self.addChild(base)
         
         
         
