e33f8078ffa225b67737c8016083059fbfd3422a
Daniel Nuttall <danielnuttall@daniels-mac-mini.local>
2015-12-11 19:17:42 +0000

    Working diagonal check.

--- Connect Four Advance/GameStatus.swift
+++ Connect Four Advance/GameStatus.swift
@@ -61,6 +61,40 @@ class GameStatus {
             }
         return "None"
         }
+    
+    // check for a winner in diagonal left to right.
+    func CheckDiaLR(array: [[Int]], col: Int, row: Int) -> String {
+        var count = 0
+        for value in -3...3{
+            if( (col+value) >= 0 && (col+value) < board.count && (row+value) >= 0 && (row+value) < board[col+value].count){
+                if(board[col+value][row+value] == Player){
+                    count++
+                    if(count == 4){
+                        return "won"
+                    }
+                    
+                }
+            }
+        }
+        return "None"
+    }
+    
+    // check for a winner in diagonal right to left.
+    func CheckDiaRL(array: [[Int]], col: Int, row: Int) -> String {
+        var count = 0
+        for value in -3...3{
+            if( (col+value) >= 0 && (col+value) < board.count && (row-value) >= 0 && (row-value) < board[col+value].count){
+                if(board[col+value][row-value] == Player){
+                    count++
+                    if(count == 4){
+                        return "won"
+                    }
+                    
+                }
+            }
+        }
+        return "None"
+    }
 
     
     
@@ -70,6 +104,8 @@ class GameStatus {
         Player = turn
         var result = ""
         var result2 = ""
+        var result3 = ""
+        var result4 = ""
         
         switch (move){
         case "buttonFactory1":
@@ -77,36 +113,50 @@ class GameStatus {
             board[0][col1count] = turn
             result = CheckCol(board, col: 0)
             result2 = CheckRow(board, col: 0,row: col1count)
+            result3 = CheckDiaLR(board, col: 0,row: col1count)
+            result4 = CheckDiaRL(board, col: 0,row: col1count)
             col1count++
         case "buttonFactory2":
             board[1][col2count] = turn
             result = CheckCol(board, col: 1)
             result2 = CheckRow(board, col: 1,row: col2count)
+            result3 = CheckDiaLR(board, col: 1,row: col2count)
+            result4 = CheckDiaRL(board, col: 1,row: col2count)
             col2count++
         case "buttonFactory3":
             board[2][col3count] = turn
             result = CheckCol(board, col: 2)
             result2 = CheckRow(board, col: 2,row: col3count)
+            result3 = CheckDiaLR(board, col: 2,row: col3count)
+            result4 = CheckDiaRL(board, col: 2,row: col3count)
             col3count++
         case "buttonFactory4":
             board[3][col4count] = turn
             result = CheckCol(board, col: 3)
             result2 = CheckRow(board, col: 3,row: col4count)
+            result3 = CheckDiaLR(board, col: 3,row: col4count)
+            result4 = CheckDiaRL(board, col: 3,row: col4count)
             col4count++
         case "buttonFactory5":
             board[4][col5count] = turn
             result = CheckCol(board, col: 4)
             result2 = CheckRow(board, col: 4,row: col5count)
+            result3 = CheckDiaLR(board, col: 4,row: col5count)
+            result4 = CheckDiaRL(board, col: 4,row: col5count)
             col5count++
         case "buttonFactory6":
             board[5][col6count] = turn
             result = CheckCol(board, col: 5)
             result2 = CheckRow(board, col: 5,row: col6count)
+            result3 = CheckDiaLR(board, col: 5,row: col6count)
+            result4 = CheckDiaRL(board, col: 5,row: col6count)
             col6count++
         case "buttonFactory7":
             board[6][col7count] = turn
             result = CheckCol(board, col: 6)
             result2 = CheckRow(board, col: 6,row: col7count)
+            result3 = CheckDiaLR(board, col: 6,row: col7count)
+            result4 = CheckDiaRL(board, col: 6,row: col7count)
             col7count++
         default: break
         }
@@ -115,7 +165,7 @@ class GameStatus {
         print("ResultRow: ",result)
         print("GAME: ",board)
         
-        if (result == "won"||result2 == "won"){
+        if (result == "won"||result2 == "won"||result3 == "won"||result4 == "won"){
             return true
         }
         return false
